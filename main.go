586e1637f53d056f553615ad48fc23412d77d89b
Andreas <a@yeat.dev>
2022-05-01 00:08:46 -0400

    add fallback
    Signed-off-by: Andreas <a@yeat.dev>

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func (m Shortlink) UnmarshalBinary(data []byte) error {
 func Run() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		fmt.Println(".env not found. falling back")
 	}
 
 	router := bunrouter.New()
